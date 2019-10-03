068e84d479e81ac4e4178a13a146928f9e9d1c6e
nachomonllor <nachomonllorc@gmail.com>
2019-10-02 21:16:25 -0300

    corrijo login2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore'; // For firebase
 import { Observable } from 'rxjs'; // For firebase
 
-import {  HostBinding } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
-
 @Component({
   selector: 'app-root',
-
-
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
