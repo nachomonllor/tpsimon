c5633b94fbe844f50035668c28c108d0b7c6d4ca
nachomonllor <nachomonllorc@gmail.com>
2019-10-03 23:06:50 -0300

    pruebas en el simon

--- src/app/simon/simon/simon.component.ts
+++ src/app/simon/simon/simon.component.ts
@@ -35,6 +35,8 @@ export class SimonComponent implements OnInit {
   encendidos :Array<string>;
 
   secuenciaUsuario:Array<number>;
+  limite:number = 15;
+  resultadoJuego:string = "secuencia completa";
 
   constructor()
   {
@@ -46,12 +48,10 @@ export class SimonComponent implements OnInit {
       
      // this.apagados = new Array("rojo", "azul", "verde", "violeta");
      // this.encendidos = new Array("rojo_claro", "azul_claro", "verde_claro", "violeta_claro");
-
       this.secuenciaUsuario = new Array<number>();
-
   }
 
-  resultadoJuego:string = "secuencia completa";
+ 
   comparar() {
      for(let i = 0; i<this.limite; i++) {
          if(this.secuencia[i] != this.secuenciaUsuario[i]) {
@@ -59,20 +59,19 @@ export class SimonComponent implements OnInit {
             break;
          }
      }
-
   }
 
-  
 
   ngOnInit() {
+
   }
 
   // mostrarSecuencia() {
-    //this.parar();
-    //this._timer = setInterval(() => this.funciones, 500);
-   // this.parar();
-   // this._timer = setInterval(() => this.apagarTodos(), 200);
-    //this.parar();
+  //this.parar();
+  //this._timer = setInterval(() => this.funciones, 500);
+  // this.parar();
+  // this._timer = setInterval(() => this.apagarTodos(), 200);
+  //this.parar();
   //}
 
   //encendidosActual:Array<string>("")
@@ -120,7 +119,7 @@ export class SimonComponent implements OnInit {
     }
    
     mostrarColor()  {
-        if(this.secuencia[ this.indiceSecuencia] == 0) {
+        if(this.secuencia[this.indiceSecuencia] == 0) {
               this.prenderRojo();
           }
           else if(this.secuencia[this.indiceSecuencia] == 1) {
@@ -150,7 +149,7 @@ export class SimonComponent implements OnInit {
        })
    }
    
-   limite:number = 15;
+   
    mostrarSecuenciaConLimite() {
     timer(this.delay, 3000).pipe(
       take(this.limite)).subscribe(x=>{
@@ -159,8 +158,6 @@ export class SimonComponent implements OnInit {
        })
      this.apagarTodos();
     // this.mostrarColor();
-
-    
    }
 
    mostrarSecuenciaUsuario() {
@@ -177,8 +174,8 @@ export class SimonComponent implements OnInit {
        for(this.limite = 1; this.limite < this.secuencia.length; this.limite++)
        { 
          this.mostrarSecuenciaConLimite();
-         this.mostrarSecuenciaUsuario();
-         this.comparar();
+         //this.mostrarSecuenciaUsuario();
+         //this.comparar();
        }
 
    }
